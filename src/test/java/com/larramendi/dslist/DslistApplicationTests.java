4dbb0f586551e5abdafc85327daa249800c38b35
Larramendi <larramendiga@gmail.com>
2023-05-08 21:22:19 -0300

    Project created

---
+++ src/test/java/com/larramendi/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larramendi.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
