d2837fa8a9a6d88b1546f28459b71e04fcd9fbe3
Gabriel Larramendi <larramendiga@gmail.com>
2023-05-10 12:16:12 -0300

    Get game by id

--- src/main/java/com/larramendi/dslist/services/GameService.java
+++ src/main/java/com/larramendi/dslist/services/GameService.java
@@ -18,7 +18,7 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
-
+    @Transactional(readOnly = true)
     public GameDTO findById(Long id) {
         Game result = gameRepository.findById(id).get();
         return new GameDTO(result);
